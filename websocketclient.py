c9bc6304278e2abff14641da0623ff751315e706
michael.jeremias <michael.jeremias.home@gmail.com>
2023-04-17 08:17:45 +0200

    cleanup

--- websocketclient.py
+++ websocketclient.py
@@ -4,65 +4,45 @@ import socketio
 
 loop = asyncio.get_event_loop()
 sio = socketio.AsyncClient()
-start_timer = None
 
 
 class SensorData(dict):
-    def __init__(self, name, subject, unit, value):
+    def __init__(self, name: str, subject: str, unit: str, value: float) -> None:
         dict.__init__(self, name=name, subject=subject, unit=unit, value=value)
 
-    def encode(self):
+    def encode(self) -> dict:
         return self.__dict__
 
 
 @sio.event
-async def connect():
+async def connect() -> None:
     await send_data()
 
 
 @sio.event
-async def on_data(sid, data):
+async def on_data(sid: str, data: str) -> None:
     print(f"from: {sid}")
     print(f"data: {data}")
 
 
 @sio.event
-async def message(message: str):
+async def message(message: str) -> None:
     print(f"websocketclient: {sio.sid}")
     print(f"message from server: {message}")
-    # await sio.emit("request_message", time.time())
 
 
-async def send_data():
-    json_object = [
-        {"name": "MH_Z19", "subject": "CO2", "unit": "ppm", "value": 567.0},
-        {
-            "name": "MH_Z19",
-            "subject": "Temperature",
-            "unit": " C",
-            "value": 24.0,
-        },
-        {"name": "MH_Z19", "subject": "Temperature", "unit": " C", "value": 24.0},
-        {
-            "name": "HTU21D",
-            "subject": "Temperature",
-            "unit": " C",
-            "value": 24.0,
-        },
-        {"name": "HTU21D", "subject": "Humidity", "unit": "%", "value": 38.0},
+async def send_data() -> None:
+    sensor_data = [
+        SensorData("MH_Z19", "CO2", "ppm", 500.0),
+        SensorData("MH_Z19", "Temperature", " C", 20.0),
     ]
-
-    json_data = [
-        SensorData("MH_Z19", "CO2", "ppm", 567.0),
-        SensorData("MH_Z19", "Temperature", " C", 24.0),
-    ]
-    jjson = json.dumps(json_data, default=lambda o: o.encode())  # .replace('"', "'")
+    sensor_data_json = json.dumps(sensor_data, default=lambda o: o.encode())
     while True:
-        await sio.emit("on_data", jjson)
+        await sio.emit("on_data", sensor_data_json)
         await sio.sleep(4)
 
 
-async def start_client():
+async def start_client() -> None:
     await sio.connect("ws://localhost:5000", wait_timeout=10)
     await sio.wait()
 
