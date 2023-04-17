3165209c8d3b07a762392301bb1181ba476141fe
michael.jeremias <michael.jeremias.home@gmail.com>
2023-04-17 09:25:49 +0200

    use random values in mock sensors

--- mocksensors.py
+++ mocksensors.py
@@ -1,17 +1,26 @@
-from mocksensors import Sensor, SensorData
+from sensor import Sensor, SensorData
+import random
 
 
 class Mock1(Sensor):
     def measure(self) -> None:
         sensor_data = []
-        sensor_data.append(SensorData(self.name, "CO2", "ppm", 528))
-        sensor_data.append(SensorData(self.name, "Temperature", " C", 24.0))
+        sensor_data.append(
+            SensorData(self.name, "CO2", "ppm", random.uniform(400.0, 1200.0))
+        )
+        sensor_data.append(
+            SensorData(self.name, "Temperature", " C", random.uniform(22.0, 24.0))
+        )
         self.sensor_data = sensor_data
 
 
 class Mock2(Sensor):
     def measure(self) -> None:
         sensor_data = []
-        sensor_data.append(SensorData(self.name, "Humidity", "%", 45.0))
-        sensor_data.append(SensorData(self.name, "Temperature", " C", 23.5))
+        sensor_data.append(
+            SensorData(self.name, "Humidity", "%", random.uniform(30.0, 60.0))
+        )
+        sensor_data.append(
+            SensorData(self.name, "Temperature", " C", random.uniform(22.0, 24.5))
+        )
         self.sensor_data = sensor_data
