2d5809214c3628918b1ade29e845e9922fd44d6d
michael.jeremias <michael.jeremias.home@gmail.com>
2023-04-28 10:24:23 +0200

    sensorstation.measure()

--- http_server.py
+++ http_server.py
@@ -7,7 +7,7 @@ from sensor.sensorstation import SensorStation
 app = Flask(__name__)
 CORS(app)
 
-DATA = [
+TEST_DATA = [
     {"name": "MH_Z19", "subject": "CO2", "unit": "ppm", "value": 1400.0},
     {"name": "MH_Z19", "subject": "temperature", "unit": " C", "value": 23.0},
     {"name": "HTU21D", "subject": "Humidity", "unit": "%", "value": 41.13811889648437},
@@ -22,13 +22,12 @@ def index():
 @app.route("/getSensorData")
 def get_sensor_data():
     # data = DATA
-    data = sensorstation.get_data_json()
-    return jsonify(data)
+    sensorstation.measure()
+    return sensorstation.get_data_json()
 
 
 if __name__ == "__main__":
     sensorstation = SensorStation()
     sensorstation.register(Htu21d("HTU21d"))
     sensorstation.register(Mhz19("MHZ19"))
-    # app.run(host="192.168.100.12", port=5000, debug=True)
-    app.run(host="raspberrypi", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
