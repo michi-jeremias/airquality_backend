bb8529073c1140fc5db567154f5f0c6a7eb2b29e
michael.jeremias <michael.jeremias.home@gmail.com>
2023-04-28 10:00:17 +0200

    let the http server send measurements

--- http_server.py
+++ http_server.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from sensor.htu21d import Htu21d
+from sensor.mhz19 import Mhz19
+from sensor.sensorstation import SensorStation
 
 app = Flask(__name__)
 CORS(app)
 
-data = [
+DATA = [
     {"name": "MH_Z19", "subject": "CO2", "unit": "ppm", "value": 1400.0},
     {"name": "MH_Z19", "subject": "temperature", "unit": " C", "value": 23.0},
     {"name": "HTU21D", "subject": "Humidity", "unit": "%", "value": 41.13811889648437},
@@ -18,8 +21,13 @@ def index():
 
 @app.route("/getSensorData")
 def get_sensor_data():
+    # data = DATA
+    data = sensorstation.get_data_json()
     return jsonify(data)
 
 
 if __name__ == "__main__":
+    sensorstation = SensorStation()
+    sensorstation.register(Htu21d("HTU21d"))
+    sensorstation.register(Mhz19("MHZ19"))
     app.run(host="192.168.100.12", port=5000, debug=True)
